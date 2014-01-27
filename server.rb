7eed10672f5029b813a82c715323d02ed3e332e8
Adrien Thery <adrien.thery@gmail.com>
2014-01-27 23:03:10 +0100

    real pin nmbers and non stubbed robot by default

--- server.rb
+++ server.rb
@@ -5,10 +5,11 @@ require 'socket'
 set :bind, '0.0.0.0'
 
 # GPIO pins numbers for left and right motors : first element in the array is forward, second is back
-pins  = { right: [4, 5], left: [6, 7] } 
+pins  = { right: [4, 18], left: [7, 8] } 
 
-# When running on an actual RPi, you should omit the ":stubbed" param like this : robot = Robot.new(pins)
-robot = Robot.new pins, :stubbed
+# When not running on an actual RPi, you should add the ":stubbed" param like this
+# robot = Robot.new pins, :stubbed
+robot = Robot.new pins
 
 robot_hostname  = Socket.gethostname
 video_feed_port = 8081
@@ -18,7 +19,7 @@ get '/' do
   erb :home, locals: {video_feed: "http://#{robot_hostname}:#{video_feed_port}"}
 end
 
-# called by XHR when the user presses one of the arrows of teh joystick to set the robot in motion
+# called by XHR when the user presses one of the arrows of the joystick to set the robot in motion
 get '/move/:direction' do
   if %w(forward back right left).include?(direction = params[:direction])
     robot.send("move_#{direction}")
